4b2f79160e51ad6be8c13fbd1f1c32f21d64f9dd
Mahathi <143727003+mahathivinayak@users.noreply.github.com>
2024-11-24 07:34:44 +0530

    done

---
+++ jackfruit/floating-multiplier/testbench.sv
@@ -0,0 +1,27 @@
+module testbench;
+    logic [31:0] a, b, y;
+
+    multiplier dut(a, b, y);
+
+    initial begin
+        a = 32'h3fc00000;
+        b = 32'h40000000;
+        #10;
+
+        a = 32'h40400000;
+        b = 32'h3f800000;
+        #10;
+
+        a = 32'h3f800000;
+        b = 32'h3f800000;
+        #10;
+
+        a = 32'h00000000;
+        b = 32'h3f800000;
+        #10;
+
+        a = 32'h7f800000;
+        b = 32'h3f800000;
+        #10;
+    end
+endmodule

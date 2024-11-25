152eae49260b9d2df0da9b40a050eeef5b544391
Mahathi <143727003+mahathivinayak@users.noreply.github.com>
2024-11-25 11:54:28 +0530

    done

---
+++ orange/e/testbench.sv
@@ -0,0 +1,14 @@
+module testbench;
+    reg [3:0] a;
+    wire [1:0] y;
+
+    priority_encoder dut (a, y);
+
+    initial begin
+        a = 4'b0000; #10;
+        a = 4'b0001; #10;
+        a = 4'b0011; #10;
+        a = 4'b0111; #10;
+        a = 4'b1xxx; #10;
+    end
+endmodule
